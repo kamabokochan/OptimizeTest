a1fbeab5239946bcf08f2bc175dddb42830d4b1a
kamabokochan <caffe@harachan.local>
2020-07-26 01:08:31 +0900

    アンチスニペット削除

--- pages/index.vue
+++ pages/index.vue
@@ -47,9 +47,9 @@ export default class IndexPage extends Vue {
   head () {
     return {
       script: [
-        {
-          innerHTML: "(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;})(window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-TLFTHCG':true});"
-        },
+        // {
+        //   innerHTML: "(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;})(window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-TLFTHCG':true});"
+        // },
         {
           src: 'https://www.googleoptimize.com/optimize.js?id=OPT-WRHD6Z3',
           onerror: 'dataLayer.hide.end && dataLayer.hide.end()'
